5f001604012b5c3a527ebf0350f5461fce755b33
Jillian Wildrick <jillianwildrick@gmail.com>
2026-08-08 23:03:20 -0500

    Add basic render loop - window opens and closes cleanly

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import pygame
 
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+
 class Game:
     def __init__(self):
         pygame.init()
         self.load_images()
         self.new_game()
+        self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.game_font = pygame.font.SysFont("Arial", 24)
+        self.main_loop()
 
     def load_images(self):
         self.images = {"robot": pygame.image.load("robot.png"), "coin": pygame.image.load("coin.png"), "monster": pygame.image.load("monster.png")}
@@ -18,7 +24,22 @@ class Game:
         while True:
             self.check_events()
 
+            self.draw_window()
+
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+
+    def draw_window(self):
+        self.window.fill((0, 0, 0))
+
+        pygame.display.flip()
+
+
+
+
+
+
+if __name__ == "__main__":
+    Game()
