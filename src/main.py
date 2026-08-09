23653a5b52b5a7cc528677a09c2f0acb769e5efc
Jillian Wildrick <jillianwildrick@gmail.com>
2026-08-08 22:26:13 -0500

    Load game images into dictionary

--- src/main.py
+++ src/main.py
@@ -3,4 +3,8 @@ import pygame
 class Game:
     def __init__(self):
         pygame.init()
+        self.load_images()
 
+
+    def load_images(self):
+        self.images = {"robot": pygame.image.load("robot.png"), "coin": pygame.image.load("coin.png"), "monster": pygame.image.load("monster.png")}
