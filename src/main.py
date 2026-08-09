b1ace18ec13fea73e005c8a9ad01891240b7f3c3
Jillian Wildrick <jillianwildrick@gmail.com>
2026-08-08 22:31:55 -0500

    Initialize game state variables

--- src/main.py
+++ src/main.py
@@ -4,7 +4,14 @@ class Game:
     def __init__(self):
         pygame.init()
         self.load_images()
+        self.new_game()
 
 
     def load_images(self):
         self.images = {"robot": pygame.image.load("robot.png"), "coin": pygame.image.load("coin.png"), "monster": pygame.image.load("monster.png")}
+
+
+    def new_game(self):
+        self.score = 0
+        self.falling_objects = []
+        #self.robot = instantiate new robot after Robot class is defined
